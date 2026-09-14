d15793183232a63c6485a7d0f7b4f4ec4feb3248
WarperSan <leumas.ecole@gmail.com>
2026-09-14 10:28:13 -0400

    Removed `isWearingGoggles()`

--- src/main/java/dev/warpersan/create_insights/api/GoggleTooltipCollector.java
+++ src/main/java/dev/warpersan/create_insights/api/GoggleTooltipCollector.java
@@ -1,22 +1,15 @@
 package dev.warpersan.create_insights.api;
 
-import com.simibubi.create.api.equipment.goggles.IHaveGoggleInformation;
-import com.simibubi.create.content.equipment.goggles.GogglesItem;
-import com.simibubi.create.content.redstone.diodes.PulseRepeaterBlockEntity;
-import com.simibubi.create.foundation.blockEntity.behaviour.scrollValue.ScrollValueBehaviour;
-import com.simibubi.create.foundation.utility.CreateLang;
+import dev.warpersan.create_insights.CreateInsights;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 
 public final class GoggleTooltipCollector
 {
@@ -48,14 +41,6 @@ public final class GoggleTooltipCollector
 		public Player player()
 		{
 			return player;
-		}
-
-		/**
-		 * Determines if the player is wearing the goggles
-		 */
-		public boolean isWearingGoggles()
-		{
-			return GogglesItem.isWearingGoggles(player);
 		}
 
 		/**
@@ -128,42 +113,6 @@ public final class GoggleTooltipCollector
 				break;
 		}
 
-		//collectGoggleInformation(tooltip, level, player, pos);
-
 		return context.compile();
-	}
-
-	private static void collectGoggleInformation(List<Component> tooltip, Level level, Player player, BlockPos pos)
-	{
-		// Only display when wearing goggles
-		if (!GogglesItem.isWearingGoggles(player))
-			return;
-
-		var blockEntity = level.getBlockEntity(pos);
-
-		// Only handle block entities
-		if (blockEntity == null)
-			return;
-
-		if (blockEntity instanceof IHaveGoggleInformation gte)
-			gte.addToGoggleTooltip(tooltip, player.isCrouching());
-
-		if (blockEntity instanceof PulseRepeaterBlockEntity pulseRepeater)
-		{
-			var builder = CreateLang.builder();
-			builder.text("owo?");
-			builder.add(CommonComponents.EMPTY);
-			builder.text(String.valueOf(pulseRepeater.getBehaviour(ScrollValueBehaviour.TYPE).value));
-
-			for (var key : pulseRepeater.getPersistentData().getAllKeys())
-			{
-				builder.text(key);
-				builder.text(" = ");
-				builder.text(Objects.requireNonNull(pulseRepeater.getPersistentData().get("key")).toString());
-				builder.add(CommonComponents.EMPTY);
-			}
-
-			builder.forGoggles(tooltip);
-		}
 	}
 }
