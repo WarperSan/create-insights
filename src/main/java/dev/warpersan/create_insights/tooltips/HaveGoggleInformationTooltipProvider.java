fe7b7ca9837a28333fc922892eda77e15322f8ee
WarperSan <leumas.ecole@gmail.com>
2026-09-14 09:53:42 -0400

    `HaveGoggleInformationTooltipProvider`

---
+++ src/main/java/dev/warpersan/create_insights/tooltips/HaveGoggleInformationTooltipProvider.java
@@ -0,0 +1,31 @@
+package dev.warpersan.create_insights.tooltips;
+
+import com.simibubi.create.api.equipment.goggles.IHaveGoggleInformation;
+import dev.warpersan.create_insights.api.GoggleTooltipCollector;
+import dev.warpersan.create_insights.api.ITooltipProvider;
+import net.minecraft.network.chat.Component;
+
+import java.util.ArrayList;
+
+public class HaveGoggleInformationTooltipProvider implements ITooltipProvider
+{
+	@Override
+	public boolean provide(GoggleTooltipCollector.TooltipContext context)
+	{
+		if (!context.isWearingGoggles())
+			return true;
+
+		var blockEntity = context.getBlockEntity();
+
+		if (!(blockEntity instanceof IHaveGoggleInformation gte))
+			return true;
+
+		var tooltip = new ArrayList<Component>();
+
+		gte.addToGoggleTooltip(tooltip, context.player().isCrouching());
+
+		context.addAll(tooltip);
+
+		return true;
+	}
+}
